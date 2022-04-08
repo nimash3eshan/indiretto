ecf6e98d70525875b6fca9f83882dca1b342437d
Ranul Navojith <ranulnavojith.99@gmail.com>
2022-04-08 15:39:28 +0530

    Restored db connection

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
-
-// Follow this pattern to import other Firebase services
-// import { } from 'firebase/<service>';
-
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAea2eh18zsrKWjcKuBCoVq8qGns-x-Xs8",
@@ -15,14 +10,33 @@ const firebaseConfig = {
   measurementId: "G-NPN7DEXW95"
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+firebase.initializeApp(firebaseConfig);
+
+
+// const app = initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
 
 // Get a list of cities from your database
-async function getCities(db) {
-  const citiesCol = collection(db, 'cities');
-  const citySnapshot = await getDocs(citiesCol);
-  const cityList = citySnapshot.docs.map(doc => doc.data());
-  return cityList;
+// async function getCities(db) {
+//   const citiesCol = collection(db, 'cities');
+//   const citySnapshot = await getDocs(citiesCol);
+//   const cityList = citySnapshot.docs.map(doc => doc.data());
+//   return cityList;
+// }
+
+// added test data to check the db connection
+function addTestData() {
+    db.collection("users").add({
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815
+    })
+        .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+        })
+        .catch((error) => {
+        console.error("Error adding document: ", error);
+        });
 }
+
