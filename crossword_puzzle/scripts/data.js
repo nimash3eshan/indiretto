f4ed00021ab70e489aa2586412952595ff8e8378
milanswanthra <86518863+milanswanthra@users.noreply.github.com>
2022-04-10 13:13:01 +0530

    Add crossword puzzle folder

---
+++ crossword_puzzle/scripts/data.js
@@ -0,0 +1,22 @@
+let data = [
+  {
+    hint5: "Stay tuned for this next clue. I have strings that can’t be tied",
+    hint13: "I always stay as still as can be. I have the power to move, but not literally",
+    hint16: "I have a spine, but no bones",
+    hint4: "I go up and down, but I never move",
+    hint6: "A band that doesn’t play instruments, but has plenty of style",
+    hint14: "If you have one, you want to share it. But once you share it, you do not have it. What is it?",
+    hint7: "Most every day, you step on me. All I require isa bend of your knee",
+    hint18: "I’m a bed without sheets, and I’m always dirty",
+    hint10: "You use knobs to make me turn colder or hotter. You’d never know it, but my head is full of water",
+    hint1: "I’m a shape and an instrument",
+    hint3: "What is always coming but never arrives?",
+    hint11: "Which room has no walls?",
+    hint9: "I’m tall when I’m young and short when I’m old",
+    hint12: "You might say I’m popular– I always have the most dates",
+    hint2: "What word is spelled incorrectly in every single dictionary?",
+    hint15: "This coat can only be put on when wet",
+    hint19: "I greet every guest, but never say a word",
+    hint8: "I start with an “e,” and I end with an “e,” but I contain only one letter",
+    hint17: "Most have clothes on the outside, but I have clothes inside"
+  }];
